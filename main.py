184091f60818024d43e311216c54abecde85625d
nearlynithin <nearlynithin@gmail.com>
2024-03-21 21:58:20 +0530

    add: duration flag and playsound

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
+#!/usr/bin/env python3
 import click
+import time
+import threading
+from playsound import playsound
 
+def play_sound():
+    while True:
+        playsound("/home/nithin/college/play/py/pomo/sounds/notification.mp3")
+
+def timer(c):
+    sound_thread = threading.Thread(target=play_sound)
+    sound_thread.daemon = True
+
+    while c > 0:
+        m, s = divmod(c, 60)
+        timer = "{:02d}:{:02d}".format(int(m), int(s))
+        print(timer, end="\r")
+        time.sleep(1)
+        c = c - 1
+    sound_thread.start()
+    key = input("Enter any key : ")
+    
 
 @click.command()
-@click.option("--name", prompt="Enter your name", help="The name of the user")
-def hello(name):
-    click.echo(f"Hello {name}!")
-
-DURATION={
-    "n": "Normal",
-    "p": "Pro",
-    "c":"Custom"
-}
-
-@click.command
-@click.argument("duration",type=click.Choice(DURATION.keys()), default="n")
-@click.option("-a","--auto",prompt="Do you need the timer to loop?", help="Infiinite pomodoro cycles")
-def pomo(duration, autostart, cycle):
-    pass
+@click.option('-d','--duration',default=25,help='Duration of pomodoro',type=int)
+@click.option('-n','name',prompt="Enter the user name",default="user",required=0)
+#@click.option("-a","--auto",prompt="Do you need the timer to loop?", help="Infiinite pomodoro cycles")
+def pomo(duration,name):
+    duration=int(duration*60)
+    timer(duration)
+    click.echo(name)
+        
+        
 if __name__ == "__main__":
-    hello()
+    pomo()
