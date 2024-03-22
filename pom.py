3058bc266324b1deccfc52b75dcae61cb8cc2dc5
nearlynithin <nearlynithin@gmail.com>
2024-03-22 19:54:37 +0530

    add: Text highlighting, Time message formatting

--- pom.py
+++ pom.py
@@ -1,43 +1,46 @@
+#!/usr/bin/env python3
+import click
 import time
-import sys
-from playsound import playsound
+from datetime import datetime
 import threading
+from playsound import playsound
+from rich import print
 
+@click.group()
+def cli():
+    pass
 
+now = datetime.now()
+formatted_time=now.strftime("%I:%M %p")
 def play_sound():
-    while True:
-        playsound("/home/nithin/college/play/py/pomo/sounds/notification.mp3")
+    playsound("/home/nithin/college/play/py/pomo/sounds/notification.mp3")
 
-
-def pomo(c):
-    sound_thread = threading.Thread(target=play_sound)
-    sound_thread.daemon = True
-
-    while c > 0:
-        m, s = divmod(c, 60)
-        timer = "{:02d}:{:02d}".format(int(m), int(s))
-        print(timer, end="\r")
+def timer(duration):
+    while duration > 0:
+        m, s = divmod(duration, 60)
+        timer_str = "{:02d}:{:02d}".format(int(m), int(s))
+        print(timer_str,end='\r')
+        #click.echo("Time Remaining: " + timer_str, nl=False)
         time.sleep(1)
-        c = c - 1
-    sound_thread.start()
-    key = input("Enter any key : ")
+        duration -= 1
+    play_sound()
 
+@cli.command()
+@click.option('-d', '--duration', default=25.0, help='Duration of pomodoro in minutes', type=float)
+@click.option('-s', '--short', default=5.0, help='Duration of short break in minutes', type=float)
+@click.option('-l', '--long', default=15.0, help='Duration of long break in minutes', type=float)
+@click.option('-c', '--cycles', default=4, help='Number of pomodoro cycles')
+def start(duration, short, long, cycles):
+    for cycle in range(1, cycles + 1):
+        print(f"[#ff85ed]Pomodoro Cycle:[/#ff85ed] {cycle}/{cycles}")
+        timer(duration * 60)
+        if cycle < cycles:
+            print("[#0ff0fc]Short Break![/#0ff0fc]")
+            timer(short * 60)
+            print(f"Ended at {formatted_time}")
+        else:
+            print("[blue]Long Break !![/blue]")
+            timer(long * 60)
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    if args:
-        function_name = args[0]
-        if function_name == pomo.__name__:
-            if len(args) >= 2:
-                try:
-                    c = float(args[1])
-                    c = int(c * 60)  # Convert minutes to seconds
-                    pomo(c)
-                except ValueError:
-                    print("Invalid argument. Please provide a numeric value for time.")
-            else:
-                print("Usage: python script.py pomo <minutes>")
-        else:
-            print("Function not found.")
-    else:
-        print("Usage: python script.py <function_name>")
+    cli()
