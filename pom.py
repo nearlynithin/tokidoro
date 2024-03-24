f2dc04155e7e488746d2ffeeccaddf6904d12cb7
nearlynithin <nearlynithin@gmail.com>
2024-03-24 11:25:06 +0530

    📝 Design elements and showconfog command

--- pom.py
+++ pom.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
 import click
+from rich import print
+from rich.console import Console
+from rich.layout import Layout
+from rich.table import Table
+from rich.panel import Panel
 import time
 from datetime import datetime
-from playsound import playsound
-from rich import print
+from pydub import AudioSegment
+from pydub.playback import play
 import json
-
+import shutil
 
 @click.group()
 def cli():
     pass
 
 now = datetime.now()
+terminal_width=int(shutil.get_terminal_size().columns)
 formatted_time=now.strftime("%I:%M %p")
+
+audio_path="/home/nithin/college/play/py/pomo/sounds/notification.mp3"
 def play_sound():
     for i in range(3):
-        playsound("/home/nithin/college/play/py/pomo/sounds/notification.mp3")
+        sound = AudioSegment.from_file(audio_path)
+        play(sound)
+    
 
 def load_config():
     try:
@@ -72,20 +82,43 @@ def start(duration, short, long, cycles):
         long=int(config["l"])
     if cycles is None:
         cycles=int(config["c"])
-    for cycle in range(1, int(cycles) + 1):
-        print(f"[#ff85ed]Pomodoro Cycle:[/#ff85ed] {cycle}/{cycles}")
-        timer(duration * 60)
-        if cycle < int(cycles):
-            print("[#0ff0fc]Short Break![/#0ff0fc]")
-            timer(short * 60)
-            print("-"*120,end='')
-            print(f"Ended at {formatted_time}")
-        else:
-            print("[blue]Long Break !![/blue]")
-            timer(long * 60)
-
-
+    while True:
+        for cycle in range(1, int(cycles) + 1):
+            print("─"*int((terminal_width/2)-9),end='')
+            print(f"[#ff85ed]Pomodoro Cycle:[/#ff85ed] {cycle}/{cycles}",end='')
+            print("─"*int((terminal_width/2)-9))
+            timer(duration * 60)
+            if cycle < int(cycles):
+                print("[#0ff0fc]Short Break![/#0ff0fc]")
+                timer(short * 60)
+                print("-"*(terminal_width-17),end='')
+                print(f"Ended at {formatted_time}")
+            else:
+                print("[blue]Long Break !![/blue]")
+                timer(long * 60)
+        new=input("do you want to start over? : (y/n) ")
+        if new=="n":
+            break
+        
+@click.command()
+def showconfig():
+    config=load_config()
+    console=Console()
+    config_string = "\n".join([
+    f"Pomodoro duration       : {config['d']}",
+    f"Short break duration    : {config['s']}",
+    f"Long break duration     : {config['l']}",
+    f"Number of cycles        : {config['c']}"
+ ])
+    console.print("[bold white]Configuration:[/bold white]")
+    console.print("┌" + "─" * (terminal_width-2) + "┐", style="blue")  
+    for line in config_string.split('\n'):
+        console.print(f"│ {line:<{terminal_width-4}} │", style="blue")
+    console.print("└" + "─" * (terminal_width-2) + "┘", style="blue")
+    
+    
 cli.add_command(start)
 cli.add_command(configure)
+cli.add_command(showconfig)
 if __name__ == "__main__":
     cli()
